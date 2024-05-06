356c593c171a0d3c2f4fdf9ea1eb2795aa84c791
Marcio Pastran <rosspastrangarcia@gmail.com>
2024-05-06 17:53:02 -0300

    preentregable2

--- app/obejt.js
+++ app/obejt.js
@@ -1,29 +1,31 @@
 //Arrays  de objetos (datos)
-const Cards = [
-    {
-        title: "Producto 1",
-        imageUrl: "assets/img/chaqueta-purpura.jpg", 
-        Content: "Chaqueta Purpura",
-        button:"Comprar",
-    },
-    {
-        title: "Producto 2",
-        imageUrl: "assets/img/buso-blanco.avif", 
-        Content: "Buso Blanco",
-        button:"Comprar",
-    },
-    {
-        title: "Producto 3",
-        imageUrl: "assets/img/buso-azul.jpg", 
-        Content: "Buso Azul",
-        button:"Comprar",
-    },
-    {
-        title: "Producto 4",
-        imageUrl: "assets/img/buso-negro.avif", 
-        Content: "Buso Negro",
-        button:"Comprar",
-    }
-];
+// const Cards = [
+//     {
+//         title: "Producto 1",
+//         imageUrl: "assets/img/chaqueta-purpura.jpg", 
+//         Content: "Chaqueta Purpura",
+//         button:"Comprar",
+//     },
+//     {
+//         title: "Producto 2",
+//         imageUrl: "assets/img/buso-blanco.avif", 
+//         Content: "Buso Blanco",
+//         button:"Comprar",
+//     },
+//     {
+//         title: "Producto 3",
+//         imageUrl: "assets/img/buso-azul.jpg", 
+//         Content: "Buso Azul",
+//         button:"Comprar",
+//     },
+//     {
+//         title: "Producto 4",
+//         imageUrl: "assets/img/buso-negro.avif", 
+//         Content: "Buso Negro",
+//         button:"Comprar",
+//     }
+// ];
 //datos guardados en localstorage
-localStorage.setItem("Cards",JSON.stringify(Cards))
+// localStorage.setItem("Cards",JSON.stringify(Cards))
+Cards = JSON.parse(localStorage.getItem("Cards"));
+console.log(Cards)
